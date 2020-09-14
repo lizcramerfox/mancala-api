60dc071576e7b60cfbd9e9fc73284a8515885f37
lizcramerfox <ecfox2008@gmail.com>
2020-09-14 14:50:51 -0400

    Rails db setup
    - Set up User & Game models/controllers/routes
    - Game model will need to be updated (placeholder)
    - Tested show/index via curl scripts

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,54 @@
+class GamesController < ProtectedController
+  # before_action: WHAT DO WE NEED TO ADD HERE FOR GAMES? i.e. FIND GAME?
+
+  # SHOW all games
+  def index
+    @games = current_user.games
+    render json: @games
+  end
+
+  def show
+    render json: @games
+  end
+
+  # CREATE new game
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      render json: @game
+    else
+      render error: { error: 'Unable to create new game. [API]' }, status: 400
+    end
+  end
+
+  # UPDATE an existing (in-progress) game
+  def update
+    if @game
+      @game.update(game_params)
+      render json: { message: 'Game progress saved.' }, status: 200
+    else
+      render json: { error: 'Unable to update game. [API]' }, status: 400
+    end
+  end
+
+  def destroy
+    if @game
+      @game.destroy
+      render json: { message: 'Game deleted.' }, status: 200
+    else
+      render json: { error: 'Unable to delete game. [API]' }, status: 400
+    end
+  end
+
+
+  private
+
+  def game_params
+    params.permit(:over).merge(user: current_user)
+  end
+
+  def find_game
+    @game = Game.find(params[:id])
+  end
+
+end
